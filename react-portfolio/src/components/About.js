d0fb95dd88a405265e929b6f9d34a644f78c077c
Alex Coulter <ALEXCOULTER.UNC@GMAIL.COM>
2020-06-23 16:10:44 -0400

    made navbar collapsible and added favicon

--- react-portfolio/src/components/About.js
+++ react-portfolio/src/components/About.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import '../App.css';
 import meImg from "../assets/images/image1.jpeg";
 import Resume from "../assets/AlexResume.pdf";
--- react-portfolio/src/components/Navbar.js
+++ react-portfolio/src/components/Navbar.js
@@ -6,8 +6,31 @@ import '../App.css';
 function Navbar() {
   return (
     <div>
-     
-      <nav className="navbar  nav justify-content-center">
+     <nav className="navbar navbar-expand-md navbar-dark">
+  <h4 className="navbar-brand">Alex Coulter</h4>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul className="navbar-nav">
+      <li className="nav-item">
+      <Link to={"/"} className="link"><p className="nav-link pt-0 pb-0" id="aboutLink" data-toggle="collapse" data-target=".navbar-collapse.show">Home</p></Link>
+      </li> 
+      <li className="nav-item">
+      <Link to={"/about/"} className="link"><p className="nav-link pt-0 pb-0" id="aboutLink" data-toggle="collapse" data-target=".navbar-collapse.show">About</p></Link>
+      </li>
+      <li className="nav-item">
+      <Link to={"/projects/"}className="link"><p className="nav-link pt-0 pb-0" id="portfolioLink" data-toggle="collapse" data-target=".navbar-collapse.show">Portfolio</p></Link>
+      </li>
+      <li className="nav-item">
+      <Link to={"/contact/"}className="link"><p className="nav-link pt-0 pb-0 lastLink" id="contactLink" data-toggle="collapse" data-target=".navbar-collapse.show">Contact</p></Link>
+      </li>    
+    </ul>
+  </div>  
+</nav>
+
+
+      {/* <nav className="navbar  nav justify-content-center">
       <Link to={"/"} className="link"><p className="nav-link pt-0 pb-0 active" id="aboutLink">Home</p></Link>
 
         <Link to={"/about/"} className="link"><p className="nav-link pt-0 pb-0" id="aboutLink">About</p></Link>
@@ -15,7 +38,7 @@ function Navbar() {
         <Link to={"/projects/"}className="link"><p className="nav-link pt-0 pb-0" id="portfolioLink">Portfolio</p></Link>
 
         <Link to={"/contact/"}className="link"><p className="nav-link pt-0 pb-0 lastLink" id="contactLink">Contact</p></Link>
-      </nav>
+      </nav> */}
     </div>
   );
 }
