9c18671aca95b4bb27c507dd8c7768a0ef25d2bc
Alex Coulter <alexcoulter.unc@gmail.com>
2024-01-06 15:13:53 -0500

    Update About.js in 2024

--- react-portfolio/src/components/About.js
+++ react-portfolio/src/components/About.js
@@ -26,8 +26,7 @@ function About() {
               <div id="picAndTextContainer">
                 <img className="me-pic img-fluid" id="mePic" src={meImg} alt="Me" />
                 <div className="skills" id="skills">
-                  <p>I'm a Full Stack Developer with extensive experience in information technology and responsive web development.  I Attended the University of North Carolina Chapel Hill where I received a BS in Psychology.  Recently, I completed
-                  a Certificate in Full Stack Web Development from UNC Chapel Hill where I learned best practices for designing powerful mobile-friendly websites and applications.  I currently work part-time as an IT Technician for a small real-estate business in Chapel Hill.
+                  <p>Dedicated Software Engineer with a passion for problem solving and a strong drive to learn. AWS certified developer with over 3 years of hands-on experience at a leading tech company, seeking new challenges to apply my skills and expertise.
                   </p>
                 </div>
               </div>
@@ -35,11 +34,11 @@ function About() {
                 <div className="bioText">
                   <h4><b>Technical Skills:</b></h4>
                   <ul>
-                    <li><b>Programming Languages:</b> HTML, CSS, Javascript</li>
-                    <li><b>Database Management:</b> Mysql, Sequelize MongoDb, Mongoose</li>
-                    <li><b>Libraries / Frameworks: </b>jQuery, React.js, Redux,  Express.js, Bootstrap,  Materialize
+                    <li><b>Programming Languages:</b>  Java, C#, HTML, CSS, JavaScript, Node.js</li>
+                    <li><b>Database Management:</b> Oracle, MySQL, Cassandra, MongoDB,  Amazon S3</li>
+                    <li><b>Libraries / Frameworks: </b> Spring, Spring Boot, React, jQuery, Express, Maven, Cloud Foundry
               </li>
-              <li><b>Other Technologies: </b>Node.js, Passport.js, Handlebars, Git, Heroku, JWT Auth, AJAX, IndexedDB API
+              <li><b>Other Technologies: </b>Passport.js, Handlebars, Git, Heroku, JWT Auth, AJAX, IndexedDB API
               </li>
                   </ul>
                   <a href={Resume} target='_blank' rel="noopener noreferrer" className="resumeLink"><button className="btn"
