0c5198ae0281205f6f6dd149d4b636906a9226bc
Alex Coulter <ALEXCOULTER.UNC@GMAIL.COM>
2024-07-08 11:56:02 -0400

    updated year in footer

--- react-portfolio/src/components/Footer.js
+++ react-portfolio/src/components/Footer.js
@@ -3,7 +3,7 @@ import '../App.css';
 
 function Footer() {
 return(
-  <footer className="footer"><span>Alex Coulter's React Portfolio &copy; 2020 </span></footer>
+  <footer className="footer"><span>Alex Coulter's React Portfolio &copy; 2024 </span></footer>
 );
 }
 
